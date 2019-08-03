bdf367afe23fd989d93459a476ecf24b7d8c2eab
neodonator <kbaral878@gmail.com>
2019-08-03 18:40:13 -0400

    starting on ejs

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-const express = require("express");
-const chalk = require("chalk");
-const debug = require("debug")("app");
-const morgan = require("morgan");
-const path = require("path");
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(morgan("tiny"));
-app.use(express.static(path.join(__dirname, "/public/")));
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.use(
-  "/css",
-  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
+  '/css',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')),
 );
 app.use(
-  "/js",
-  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')),
 );
-app.use("/js", express.static(path.join(__dirname, "/node_modules/jquery/js")));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/js')));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { list: ['a', 'b'], title: 'library' });
 });
 
 app.listen(port, () => {

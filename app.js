d2428d979548faf5693fda162bbb332adaa3c580
neodonator <kbaral878@gmail.com>
2019-08-03 23:43:51 -0400

    changed the booklist

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
@@ -22,8 +23,61 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+const books = [
+  {
+    title: 'journey to the center of the earth',
+    id: 1,
+    author: 'Mark Twain',
+    description: 'Science Fiction'
+  },
+  {
+    title: 'Karnali Blues',
+    id: 2,
+    author: 'Buddi Sagar',
+    description: 'Nepali Classic'
+  },
+  {
+    title: 'Dear Sufi',
+    id: 3,
+    author: 'Subin Bhattarai',
+    description: 'Love Story'
+  },
+];
+
+app.use('/books', bookRouter);
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('hello single book');
+  });
+
+bookRouter.route('/')
+  .get((req, res) => {
+    res.render(
+      'books',
+      {
+        nav: [
+          { links: '/books', title: 'Books' },
+          { links: '/authors', title: 'Author' }
+        ],
+        title: 'Library',
+        books
+      },
+    );
+  });
+
 app.get('/', (req, res) => {
-  res.render('index', { list: ['a', 'b'], title: 'Library' });
+  res.render(
+    'index',
+    {
+      nav: [
+        { links: '/books', title: 'Books' },
+        { links: '/authors', title: 'Author' }
+      ],
+      title: 'Library',
+      books
+    },
+  );
 });
 
 app.listen(port, () => {
